2efb65f6ff25d53e777b49fe00a43c9de2bbdb6f
ElderGr <gbielrs@gmail.com>
2022-09-05 23:24:28 -0300

    feat: added footer basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { QuestionsListComponent } from './questions-list/questions-list.component';
 import { QuestionComponent } from './question/question.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     QuestionsListComponent,
-    QuestionComponent
+    QuestionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
