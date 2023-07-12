ea6f66c4ba06f0416b4e71c5d66a7d2c3e6e655c
Rodrigodcs <rodrigodcsee@gmail.com>
2023-07-12 10:54:19 -0300

    Code review

--- src/app/questions-list/questions-list.constants.ts
+++
@@ -1,6 +0,0 @@
-export const defaultQuestionsValues = [
-  { id: 1, text: 'O que é JSX?', awnser: 'JSX é uma sintaxe para escrever HTML dentro do JS', isRight: null},
-  { id: 2, text: 'O que é React?', awnser: 'JSX é uma sintaxe para escrever HTML dentro do JS', isRight: null},
-  { id: 3, text: 'O que é Virtual DOM?', awnser: 'JSX é uma sintaxe para escrever HTML dentro do JS', isRight: null},
-  { id: 4, text: 'O que é React Native?', awnser: 'JSX é uma sintaxe para escrever HTML dentro do JS', isRight: null},
-]
